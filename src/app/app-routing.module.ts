ff8366229c2f1c81c19da011c431c0750d2be736
GabriielDeV <xavier.gabriiell@gmail.com>
2025-02-08 20:56:24 -0300

    Ajustes para GitHub Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '**', redirectTo: '' }
-];
+const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    useHash: true // Adiciona hash para melhor compatibilidade com GitHub Pages
-  })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
